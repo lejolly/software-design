1077d7062fec0763496c79ab45260e51a04e862f
Joshua Lee <me@jolly.sg>
2016-03-14 19:32:02 +0100

    initial version of ColourPicker with ColourBean

---
+++ src/main/java/tools/ColourBean.java
@@ -0,0 +1,32 @@
+package tools;
+
+import javax.swing.event.SwingPropertyChangeSupport;
+import java.awt.*;
+import java.beans.PropertyChangeListener;
+
+public class ColourBean {
+
+    private SwingPropertyChangeSupport swingPropertyChangeSupport = new SwingPropertyChangeSupport(this);
+    private Color colour = Color.BLACK;
+
+    public ColourBean(PropertyChangeListener propertyChangeListener) {
+        addPropertyChangeListener(propertyChangeListener);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+        this.swingPropertyChangeSupport.addPropertyChangeListener(propertyChangeListener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+        this.swingPropertyChangeSupport.removePropertyChangeListener(propertyChangeListener);
+    }
+
+    public Color getColour() {
+        return colour;
+    }
+
+    public void setColour(Color colour) {
+        this.swingPropertyChangeSupport.firePropertyChange("Colour", this.colour, colour);
+        this.colour = colour;
+    }
+}
